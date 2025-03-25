a2a79a3a85c56c2e46320eef3e468748b11e544d
charlesanthony1996 <charlesanthony296@gmail.com>
2025-03-25 11:59:18 +0100

    adaline implementation

--- chapter_3.py
+++ chapter_3.py
@@ -85,8 +85,8 @@ import numpy as np
 def sigmoid(z):
     return 1.0 / (1.0 + np.exp(-z))
 
-z = np.arange(-7, 7, 0.1)
-sigma_z = sigmoid(z)
+# z = np.arange(-7, 7, 0.1)
+# sigma_z = sigmoid(z)
 # plt.plot(z, sigma_z)
 # plt.axvline(0.0, color='k')
 # plt.ylim(-0.1, 1.1)
@@ -99,4 +99,29 @@ sigma_z = sigmoid(z)
 # plt.show()
 
 
+# short code snippet to create a plot that illustrates the loss of classifying a single training example for different
+# values of sigma(z)
+
+def loss_1(z):
+    return - np.log(sigmoid(z))
+
+def loss_0(z):
+    return - np.log(1 - sigmoid(z))
+
+# z = np.arange(-10, 10, 0.1)
+# sigma_z = sigmoid(z)
+# c1 = [loss_1(x) for x in z]
+# plt.plot(sigma_z, c1, label='L(w, b) if y=1')
+# c0 = [loss_0(x) for x in z]
+# plt.plot(sigma_z, c0, linestyle='--', label='L(w, b) if y=0')
+# plt.ylim(0.0, 5.1)
+# plt.xlim([0, 1])
+# plt.xlabel('$\sigma(z)$')
+# plt.ylabel('L(w, b)')
+# plt.legend(loc='best')
+# plt.tight_layout()
+# plt.show()
+
+
+# converting an adaline implementation into an algorithm for logistic regression
 
