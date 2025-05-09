5a85ae1e35bfc302583bc251693a77ce9d1551e6
charlesanthony1996 <charlesanthony296@gmail.com>
2025-05-09 20:57:50 +0200

    plot of l1 regularization use in wine dataset

--- chapter_4.py
+++ chapter_4.py
@@ -156,8 +156,8 @@ print(df_wine.head())
 
 from sklearn.model_selection import train_test_split
 
-x, y = df_wine.iloc[:, 1].values, df_wine.iloc[:, 0].values
-x_train , x_test, y_train , y_test = train_test_split(x, y, test_size=0.3, stratify=y)
+x, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+x_train , x_test, y_train , y_test = train_test_split(x, y, test_size=0.3,random_state=0, stratify=y)
 
 # min max scaling procedure
 
@@ -165,7 +165,60 @@ from sklearn.preprocessing import MinMaxScaler
 
 mms = MinMaxScaler()
 x_train_norm = mms.fit_transform(x_train)
-x_test_norm = mms.fit_transform(x_test)
+x_test_norm = mms.transform(x_test)
 
-# example
+# examples of standardization and normalization
+ex = np.array([0, 1, 2, 3, 4, 5])
+print("standardized: ", (ex - ex.mean()) / ex.std())
+print("normalized: ", (ex - ex.min()) / (ex.max() - ex.min()))
 
+
+from sklearn.preprocessing import StandardScaler
+stdsc = StandardScaler()
+
+x_train_std = stdsc.fit_transform(x_train)
+x_test_std = stdsc.transform(x_test)
+
+from sklearn.linear_model import LogisticRegression
+LogisticRegression(penalty='l1', solver='liblinear', multi_class='ovr')
+
+lr = LogisticRegression(penalty='l1', C=1.0, solver='liblinear', multi_class='ovr')
+lr.fit(x_train_std, y_train)
+
+print("Training accuracy: ", lr.score(x_train_std, y_train))
+print("Testing accuracy: ", lr.score(x_test_std, y_test))
+
+# intercept
+
+print("Intercept: ", lr.intercept_)
+print("coefficient:", lr.coef_)
+
+# regularization strength example
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+
+ax = plt.subplot(111)
+colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'pink', 'lightgreen', 'lightblue',
+           'gray', 'indigo', 'orange']
+weights, params = [], []
+
+for c in np.arange(-4., 6.):
+    lr = LogisticRegression(penalty='l1', C=10.**c, solver='liblinear', multi_class='ovr', random_state=0)
+
+    lr.fit(x_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10**c)
+
+weights = np.array(weights)
+for column, color in zip(range(weights.shape[1]), colors):
+    plt.plot(params, weights[:, column], label=df_wine.columns[column+ 1], color=color)
+
+plt.axhline(0, color='black', linestyle='--', linewidth=3)
+plt.xlim([10**(-5), 10**5])
+plt.ylabel('Weight coefficient')
+plt.xlabel('C (inverse regularization strength)')
+plt.xscale('log')
+plt.legend(loc='upper left')
+ax.legend(loc='upper center', bbox_to_anchor=(1.38, 1.03), ncol=1, fancybox=True)
+# plt.show()
