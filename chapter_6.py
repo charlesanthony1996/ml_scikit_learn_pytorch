dabb56f7d40cf877af38a56d9d7fafab9aff7f15
charlesanthony1996 <charlesanthony296@gmail.com>
2025-05-15 09:22:28 +0200

    stratified k fold validation

--- chapter_6.py
+++ chapter_6.py
@@ -16,11 +16,27 @@ print(le.transform(['M', 'B']))
 
 from sklearn.model_selection import train_test_split
 
-x_train, x_train, y_train, y_test = train_test_split(x, y, test_size=0.20, stratify=y, random_state=1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, stratify=y, random_state=1)
 print(x_train.shape)
 print(len(x_train))
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-from sklearn.
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+
+pipe_lr = make_pipeline(StandardScaler(), PCA(n_components=2), LogisticRegression())
+pipe_lr.fit(x_train, y_train)
+y_pred = pipe_lr.predict(x_test)
+test_acc = pipe_lr.score(x_test, y_test)
+print(f"Test Accuracy: {test_acc:.3f}")
+
+
+# stratified k fold validation
+
+import numpy as np
+from sklearn.model_selection import StratifiedGroupKFold
+
+
+
 
