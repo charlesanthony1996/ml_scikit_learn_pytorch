33d8179fe223ff8bfc4943f41f5511d00bd710d0
charlesanthony1996 <charlesanthony296@gmail.com>
2025-05-22 10:35:06 +0200

    successive halving

--- chapter_6.py
+++ chapter_6.py
@@ -111,3 +111,64 @@ plt.ylim([0.8, 1.0])
 # plt.show()
 
 # tuning hyperparameters via grid search
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+
+pipe_svc = make_pipeline(StandardScaler(), SVC(random_state= 1))
+param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+
+param_grid = [{'svc__C': param_range, 'svc__kernel': ['linear']},{'svc__C': param_range, 'svc__gamma': param_range, 'svc__kernel': ['rbf']}]
+
+gs = GridSearchCV(estimator=pipe_svc, param_grid=param_grid, scoring='accuracy', cv=10, refit=True, n_jobs=-1)
+gs = gs.fit(x_train, y_train)
+print(gs.best_score_)
+
+print(gs.best_params_)
+
+clf = gs.best_estimator_
+clf.fit(x_train, y_train)
+print(f'Test accuracy: , {clf.score(x_test, y_test):.3f}')
+
+# exploring hyperparameter configurations more widely with randomized search
+
+param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+
+import scipy
+param_range = scipy.stats.loguniform(0.0001, 1000.0)
+
+np.random.seed(1)
+param_range.rvs(10)
+
+
+from sklearn.model_selection import RandomizedSearchCV
+
+pipe_svc = make_pipeline(StandardScaler(), SVC(random_state=1))
+param_grid = [{'svc__C': param_range, 'svc__kernel': ['linear']}, {'svc__C': param_range, 
+                                                                   'svc__gamma': param_range, 'svc__kernel': ['rbf']}]
+
+rs = RandomizedSearchCV(
+    estimator=pipe_svc, 
+    param_distributions=param_grid,
+    scoring='accuracy',
+    refit=True,
+    n_iter=20,
+    cv=10,
+    random_state=1,
+    n_jobs=-1
+)
+
+
+rs = rs.fit(x_train, y_train)
+print(rs.best_score_)
+
+print(rs.best_params_)
+
+
+# more resource efficient hyperparameter search with successive halving
+
+from sklearn.experimental import enable_halving_search_cv
+
+
+from sklearn.model_selection import HalvingGridSearchCV
+
