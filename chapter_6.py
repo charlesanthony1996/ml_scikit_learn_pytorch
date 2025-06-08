9b6ad4d303e57652f01484aa72e6a9591bab3e65
charlesanthony1996 <charlesanthony296@gmail.com>
2025-06-08 20:07:07 +0200

    single majority vote classifier

--- chapter_6.py
+++ chapter_6.py
@@ -323,3 +323,26 @@ y_imb = np.hstack((y[y == 0], y[y == 1][:40]))
 y_pred = np.zeros(y_imb.shape[0])
 np.mean(y_pred == y_imb) * 100
 
+print(np.mean(y_pred == y_imb) * 100)
+
+
+from sklearn.utils import resample
+print('Number of class 1 examples before: ', x_imb[y_imb == 1].shape[0])
+
+x_upsampled, y_upsampled = resample(
+    x_imb[y_imb == 1],
+    y_imb[y_imb == 1],
+    replace=True,
+    n_samples=x_imb[y_imb == 0].shape[0],
+    random_state=123
+)
+
+print('Number of class 1 examples after: ',x_upsampled.shape[0])
+
+x_bal = np.vstack((x[y == 0], x_upsampled))
+y_bal = np.hstack((y[y == 0], y_upsampled))
+
+y_pred = np.zeros(y_bal.shape[0])
+np.mean(y_pred == y_bal) * 100
+
+print(np.mean(y_pred == y_bal) * 100)
