3ed205eebe55f9853f51622b0857f4b6b04c7c8e
charlesanthony1996 <charlesanthony296@gmail.com>
2025-07-08 09:47:52 +0200

    majority vote classifier class

--- chapter_7.py
+++ chapter_7.py
@@ -53,3 +53,75 @@ from sklearn.pipeline import _name_estimators
 import numpy as np
 import operator
 
+class majorityvoterclassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, classifiers, vote='classlabel', weights=None):
+        self.classifiers = classifiers
+        self.named_classifiers = {
+            key: value for key,
+            value in _name_estimators(classifiers)
+        }
+        self.vote = vote
+        self.weights = weights
+
+    def fit(self, x, y):
+        if self.vote not in ('probability', 'classlabel'):
+            raise ValueError(f"'vote must be probability'"
+                             f"or 'classlabel'"
+                             f'; got {len(self.weights)} weights, '
+                             f' {len(self.classifiers)} classifiers')
+        
+        self.labelenc_ = LabelEncoder()
+        self.labelenc_.fit(y)
+        self.classes_ = self.labelenc_.classes_
+        self.classifiers_ = []
+        for clf in self.classifiers:
+            fitted_clf = clone(clf).fit(x, self.labelenc_.transform(y))
+            self.classifiers_.append(fitted_clf)
+        return self
+    
+
+    def predict(self, x):
+        if self.vote == 'probability':
+            maj_vote = np.argmax(self.predict_proba(x), axis= 1)
+        else:
+            predictions = np.asarray([
+                clf.predict(x) for clf in self.classifiers_
+            ]).T
+
+            maj_vote = np.apply_along_axis(
+                lambda x: np.argmax(
+                    np.bincount(x, weights=self.weights)
+                ),
+                axis = 1, arr=predictions
+            )
+        maj_vote = self.labelenc_.inverse_transform(maj_vote)
+        return maj_vote
+    
+    def predict_proba(self, x):
+        probas = np.asarray([clf.predict_proba(x) for clf in self.classfiers_])
+        avg_proba = np.average(probas, axis=0, weights=self.weights)
+        return avg_proba
+    
+    def get_params(self, deep=True):
+        if not deep:
+            return super().get_params(deep=False)
+        else:
+            out = self.named_classifiers.copy()
+            for name, step in self.named_classifiers.items():
+                for key, value in step.params(deep=True).items():
+                    out[f'{name}___{key}'] = value
+            return out
+        
+
+
+
+
+
+# using the majority voting principle to make predictions
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+
+
+iris = datasets.load_iris()
